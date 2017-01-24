843113bc17cbd33709d0b5f8915be9f959d8e8c7
lotrik <lotrik@mail.ru>
2017-01-24 21:11:39 +0200

    Buffer

--- app/src/main/java/org/itstep/pastukhov/internetradio/Playback.java
+++ app/src/main/java/org/itstep/pastukhov/internetradio/Playback.java
@@ -3,11 +3,9 @@ package org.itstep.pastukhov.internetradio;
 
 import android.content.Context;
 import android.net.Uri;
-import android.os.Handler;
 import android.util.Log;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -15,19 +13,13 @@ import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultAllocator;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-import java.io.IOException;
 
 /**
  * Created by lotrik on 15.01.17.
@@ -37,46 +29,35 @@ public class Playback {
 
     Context mContext;
 
-    SimpleExoPlayerView mSimpleExoPlayerView;
-
-    public void startPlay(Context context, SimpleExoPlayerView simpleExoPlayerView) {
+    public void startPlay(Context context) {
 
         mContext = context;
-        mSimpleExoPlayerView = simpleExoPlayerView;
 
         Uri stream = Uri.parse("http://online-radioroks.tavrmedia.ua/RadioROKS");
 
-        Handler mainHandler = new Handler();
-
-        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
-
-        TrackSelection.Factory videoTrackSelectionFactory =
-                new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+//        Handler mainHandler = new Handler();
 
         TrackSelector trackSelector =
-                new DefaultTrackSelector(videoTrackSelectionFactory);
+                new DefaultTrackSelector();
 
-        LoadControl loadControl = new DefaultLoadControl(new DefaultAllocator(true, 1000), 3000,
-                100000, 2000, 2000);
+        LoadControl loadControl = new DefaultLoadControl(new DefaultAllocator(true, 1000), 5000,
+                10000, 5000, 5000);
 
         SimpleExoPlayer player =
-                ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+                ExoPlayerFactory.newSimpleInstance(mContext, trackSelector, loadControl);
 
-        simpleExoPlayerView.setPlayer(player);
-
-        // Measures bandwidth during playback. Can be null if not required.
-        DefaultBandwidthMeter defaultBandwidthMeter = new DefaultBandwidthMeter();
-// Produces DataSource instances through which media data is loaded.
         DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(mContext,
-                Util.getUserAgent(mContext, "Internet Radio"), defaultBandwidthMeter);
-// Produces Extractor instances for parsing the media data.
+                Util.getUserAgent(mContext, "Internet Radio"), null);
+
         ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
-// This is the MediaSource representing the media to be played.
-        MediaSource videoSource = new ExtractorMediaSource(stream,
-                dataSourceFactory, extractorsFactory, mainHandler, null);
-// Prepare the player with the source.
-        player.prepare(videoSource);
-        //player.setPlayWhenReady(true);
+
+        MediaSource audioSource = new ExtractorMediaSource(stream,
+                dataSourceFactory, extractorsFactory, null, null);
+
+        player.prepare(audioSource);
+        player.setPlayWhenReady(true);
+
+        Log.d ("Buffer", "Buffer: " + player.getBufferedPercentage() + "%");
 
     }
 
